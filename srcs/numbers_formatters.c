1cd36ba11ec42a90ae7ad9798f8d128cdfb4ac44
elodie <elodie.lim@edu.esiee.fr>
2024-12-29 02:52:59 +0100

    update

--- srcs/numbers_formatters.c
+++ srcs/numbers_formatters.c
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   numbers_formatters.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elodlim <elodlim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/28 03:36:48 by elodlim           #+#    #+#             */
-/*   Updated: 2024/12/28 03:36:48 by elodlim          ###   ########.fr       */
+/*   Created: 2024/12/29 02:49:28 by elodlim           #+#    #+#             */
+/*   Updated: 2024/12/29 02:49:28 by elodlim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include <stdarg.h>
-# include <unistd.h>
-# include <limits.h>
-# include <stddef.h>
-# include <stdint.h>
-# include <stdlib.h>
-# include <stdio.h>
-
-#endif
